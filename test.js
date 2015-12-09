b818f1f7b03a08df9b4739123ae558dd067e5815
Ahmad Ben Mrad <batikhsouri@gmail.com>
2015-12-09 10:26:45 +0100

    Fuzzing tests are optional

--- test.js
+++ test.js
@@ -3,7 +3,15 @@ var crypto = require('crypto');
 var assert = require('assert');
 // NOTE : when you have installed node-cryptopp, use require('cryptopp') instead
 
-var yell = process.argv.length > 2 && process.argv[2] == 'verbose';
+var useFuzzing = false;
+var yell = false;
+
+if (process.argv.length > 2){
+	for (var i = 2; i < process.argv.length; i++){
+		if (process.argv[i] == 'verbose') yell = true;
+		else if (process.argv[i] == 'fuzzing') useFuzzing = true;
+	}
+}
 
 log('CRYPTOPP TEST AND EXAMPLE SCRIPT');
 
@@ -59,34 +67,36 @@ assert.deepEqual(isRsaSignValid, true, 'The RSA signature is invalid');
 assert.deepEqual(otherIsRsaSignValid, false, 'RSA signatures do not work!');
 assert.deepEqual(fuzzingRsaValid, false, 'RSA signatures can spoofed with fuzzing!');
 
-log('RSA fuzzing test : generating random data and passing it through RSA methods to check that exceptions are raised');
-function RsaEncFuzzing(){
-	cryptopp.rsa.encrypt(rsaTest, rand(), rand());
+if (useFuzzing){
+	log('RSA fuzzing test : generating random data and passing it through RSA methods to check that exceptions are raised');
+	function RsaEncFuzzing(){
+		cryptopp.rsa.encrypt(rsaTest, rand(), rand());
+	}
+	function RsaDecFuzzing(){
+		cryptopp.rsa.decrypt(rand(), rand(), rand(), rand());
+	}
+	function RsaSigFuzzing(){
+		cryptopp.rsa.sign(rsaSignTest, rand(), rand(), rand());
+	}
+	function RsaVerFuzzing(){
+		var isValid = cryptopp.rsa.verify(rsaSignTest, rand(), rand(), rand());
+		assert.deepEqual(isValid, false, 'Random data was accepted as valid RSA signature!!!');
+	}
+	log('Fuzzing RSA encryption');
+	try {
+		RsaEncFuzzing();
+		console.log('RSA encryption fuzzing didn\'t raise an exception')
+	} catch (e){}
+	log('Fuzzing RSA decryption');
+	assert.throws(RsaDecFuzzing, Error, 'RSA decryption fuzzing didn\'t raise an exception');
+	log('Fuzzing RSA signature');
+	assert.throws(RsaSigFuzzing, Error, 'RSA signature fuzzing didn\'t raise an exception');
+	log('Fuzzing RSA signature verification');
+	try { //RSA signature verification doesn't always throw an exception when random data is thrown at it
+		RsaVerFuzzing();
+		console.log('RSA signature verification fuzzing didn\'t raise an exception');
+	} catch (e){}
 }
-function RsaDecFuzzing(){
-	cryptopp.rsa.decrypt(rand(), rand(), rand(), rand());
-}
-function RsaSigFuzzing(){
-	cryptopp.rsa.sign(rsaSignTest, rand(), rand(), rand());
-}
-function RsaVerFuzzing(){
-	var isValid = cryptopp.rsa.verify(rsaSignTest, rand(), rand(), rand());
-	assert.deepEqual(isValid, false, 'Random data was accepted as valid RSA signature!!!');
-}
-log('Fuzzing RSA encryption');
-try {
-	RsaEncFuzzing();
-	console.log('RSA encryption fuzzing didn\'t raise an exception')
-} catch (e){}
-log('Fuzzing RSA decryption');
-assert.throws(RsaDecFuzzing, Error, 'RSA decryption fuzzing didn\'t raise an exception');
-log('Fuzzing RSA signature');
-assert.throws(RsaSigFuzzing, Error, 'RSA signature fuzzing didn\'t raise an exception');
-log('Fuzzing RSA signature verification');
-try { //RSA signature verification doesn't always throw an exception when random data is thrown at it
-	RsaVerFuzzing();
-	console.log('RSA signature verification fuzzing didn\'t raise an exception');
-} catch (e){}
 
 // Testing DSA signature and verification
 var dsaTest = "testing DSA signature scheme";
@@ -103,25 +113,26 @@ assert.deepEqual(dsaIsValid, true, 'The DSA signature is invalid');
 assert.deepEqual(otherDsaIsValid, false, 'DSA signatures do not work!');
 assert.deepEqual(fuzzingDsaValid, false, 'DSA signatures can be spoofed with fuzzing!');
 
-/*
-log('DSA fuzzing test : generating random data and passing it through DSA methods to check that exceptions are raised');
-function dsaSigFuzzing(){
-	cryptopp.dsa.sign(dsaTest, rand(), rand(), rand(), rand());
+if (useFuzzing){
+	log('DSA fuzzing test : generating random data and passing it through DSA methods to check that exceptions are raised');
+	function dsaSigFuzzing(){
+		cryptopp.dsa.sign(dsaTest, rand(), rand(), rand(), rand());
+	}
+	function dsaVerFuzzing(){
+		var isValid = cryptopp.dsa.verify(dsaTest, rand(), rand(), rand(), rand(), rand());
+		assert.deepEqual(isValid, false, 'Random data was accepted as valid DSA signature!!!');
+	}
+	log('Fuzzing signature');
+	try {
+		dsaSigFuzzing();
+		log('DSA signature fuzzing didn\'t raise an exception');
+	} catch (e){}
+	log('Fuzzing siganture verification');
+	try {
+		dsaVerFuzzing();
+		log('DSA verification fuzzing didn\'t raise an exception');
+	} catch (e){}
 }
-function dsaVerFuzzing(){
-	var isValid = cryptopp.dsa.verify(dsaTest, rand(), rand(), rand(), rand(), rand());
-	assert.deepEqual(isValid, false, 'Random data was accepted as valid DSA signature!!!');
-}
-log('Fuzzing signature');
-try {
-	dsaSigFuzzing();
-	log('DSA signature fuzzing didn\'t raise an exception');
-} catch (e){}
-log('Fuzzing siganture verification');
-try {
-	dsaVerFuzzing();
-	log('DSA verification fuzzing didn\'t raise an exception');
-} catch (e){}*/
 
 // Testing ECIES encryption/decryption
 var eciesTest = "Testing ECIES encryption/decryption";
@@ -133,26 +144,28 @@ var eciesDecrypted = cryptopp.ecies.prime.decrypt(eciesCipher, eciesKeyPair.priv
 log("Plain text (decrypted) : " + eciesDecrypted);
 assert.equal(eciesTest, eciesDecrypted, 'The decrypted ECIES message is invalid (prime fields)');
 
-/*
-function eciesPrimeKeyPairFuzzing(){
-	cryptopp.ecies.prime.generateKeyPair(rand());
-}*/
-function eciesPrimeEncFuzzing(){
-	cryptopp.ecies.prime.encrypt(eciesTest, {x: rand(), y: rand()}, 'secp256r1');
-}
-function eciesPrimeDecFuzzing(){
-	cryptopp.ecies.prime.decrypt(eciesCipher, rand(), 'secp256r1');
-}
+if (useFuzzing){
+	/*
+	function eciesPrimeKeyPairFuzzing(){
+		cryptopp.ecies.prime.generateKeyPair(rand());
+	}*/
+	function eciesPrimeEncFuzzing(){
+		cryptopp.ecies.prime.encrypt(eciesTest, {x: rand(), y: rand()}, 'secp256r1');
+	}
+	function eciesPrimeDecFuzzing(){
+		cryptopp.ecies.prime.decrypt(eciesCipher, rand(), 'secp256r1');
+	}
 
-//eciesPrimeKeyPairFuzzing();
-try {
-	eciesPrimeEncFuzzing();
-	console.log('ECIES encryption (with fuzzing) on prime curve didn\'t raise an exception');
-} catch (e){}
-try {
-	eciesPrimeDecFuzzing();
-	console.log('ECIES decryption (with fuzzing) on prime curve didn\'t raise an exception');
-} catch (e){}
+	//eciesPrimeKeyPairFuzzing();
+	try {
+		eciesPrimeEncFuzzing();
+		console.log('ECIES encryption (with fuzzing) on prime curve didn\'t raise an exception');
+	} catch (e){}
+	try {
+		eciesPrimeDecFuzzing();
+		console.log('ECIES decryption (with fuzzing) on prime curve didn\'t raise an exception');
+	} catch (e){}
+}
 
 //Testing ECIES on binary fields
 eciesKeyPair = cryptopp.ecies.binary.generateKeyPair('sect283r1');
@@ -163,25 +176,27 @@ eciesDecrypted = cryptopp.ecies.binary.decrypt(eciesCipher, eciesKeyPair.private
 log('Plain text (decrypted) : ' + eciesDecrypted);
 assert.equal(eciesTest, eciesDecrypted, 'The decrypted ECIES message is invalid (binary fields)');
 
-/*function eciesBinaryKeyPairFuzzing(){
-	cryptopp.ecies.binary.generateKeyPair(rand());
-}*/
-function eciesBinaryEncFuzzing(){
-	cryptopp.ecies.binary.encrypt(eciesTest, {x: rand(), y: rand()}, 'sect283r1');
-}
-function eciesBinaryDecFuzzing(){
-	cryptopp.ecies.binary.decrypt(eciesCipher, rand(), 'sect283r1');
-}
+if (useFuzzing){
+	/*function eciesBinaryKeyPairFuzzing(){
+		cryptopp.ecies.binary.generateKeyPair(rand());
+	}*/
+	function eciesBinaryEncFuzzing(){
+		cryptopp.ecies.binary.encrypt(eciesTest, {x: rand(), y: rand()}, 'sect283r1');
+	}
+	function eciesBinaryDecFuzzing(){
+		cryptopp.ecies.binary.decrypt(eciesCipher, rand(), 'sect283r1');
+	}
 
-//eciesBinaryKeyPairFuzzing();
-try {
-	eciesBinaryEncFuzzing();
-	console.log('ECIES encryption (with fuzzing) on binary curve didn\'t raise an exception');
-} catch (e){}
-try {
-	eciesBinaryDecFuzzing();
-	console.log('ECIES decryption (with fuzzing) on binary curve didn\'t raise an exception');
-} catch (e){}
+	//eciesBinaryKeyPairFuzzing();
+	try {
+		eciesBinaryEncFuzzing();
+		console.log('ECIES encryption (with fuzzing) on binary curve didn\'t raise an exception');
+	} catch (e){}
+	try {
+		eciesBinaryDecFuzzing();
+		console.log('ECIES decryption (with fuzzing) on binary curve didn\'t raise an exception');
+	} catch (e){}
+}
 
 //Testing ECDSA signing and verification on prime fields
 var ecdsaTest = "testing ECDSA signing and verification";
@@ -198,26 +213,27 @@ assert.deepEqual(ecdsaIsValid, true, 'The ECDSA signature is invalid (prime fiel
 assert.deepEqual(ecdsaIsNotValid, false, 'ECDSA signatures verification does not work!!!');
 assert.deepEqual(fuzzingEcdsaValid, false, 'ECDSA signatures can be spoofed with fuzzing!');
 
+if (useFuzzing){
+	/*function ecdsaPrimeKeyPairFuzzing(){
+		cryptopp.ecdsa.prime.createKeyPair(rand());
+	}*/
+	function ecdsaPrimeSigFuzzing(){
+		cryptopp.ecdsa.prime.sign(ecdsaTest, rand(), 'secp256r1');
+	}
+	function ecdsaPrimeVerFuzzing(){
+		var isValid = cryptopp.ecdsa.prime.verify(ecdsaTest, rand(), {x: rand(), y: rand()}, 'secp256r1');
+		assert.deepEqual(isValid, false, 'Random data was accepted as valid ECDSA siganture!!!');
+	}
 
-/*function ecdsaPrimeKeyPairFuzzing(){
-	cryptopp.ecdsa.prime.createKeyPair(rand());
-}*/
-function ecdsaPrimeSigFuzzing(){
-	cryptopp.ecdsa.prime.sign(ecdsaTest, rand(), 'secp256r1');
+	try {
+		ecdsaPrimeSigFuzzing();
+		console.log('ECDSA signature (with fuzzing) on prime curve didn\'t raise an exception');
+	} catch (e){}
+	try {
+		ecdsaPrimeVerFuzzing();
+		console.log('ECDSA verification (with fuzzing) on prime curve didn\'t raise on exception');
+	} catch(e){}
 }
-function ecdsaPrimeVerFuzzing(){
-	var isValid = cryptopp.ecdsa.prime.verify(ecdsaTest, rand(), {x: rand(), y: rand()}, 'secp256r1');
-	assert.deepEqual(isValid, false, 'Random data was accepted as valid ECDSA siganture!!!');
-}
-
-try {
-	ecdsaPrimeSigFuzzing();
-	console.log('ECDSA signature (with fuzzing) on prime curve didn\'t raise an exception');
-} catch (e){}
-try {
-	ecdsaPrimeVerFuzzing();
-	console.log('ECDSA verification (with fuzzing) on prime curve didn\'t raise on exception');
-} catch(e){}
 
 //Testing ECDSA signing and verification on binary fields
 /*ecdsaKeyPair = cryptopp.ecdsa.binary.generateKeyPair('sect283r1');
@@ -241,13 +257,15 @@ assert.equal(secret1, secret2, 'The shared secret isn\'t the same (prime fields)
 log("Secret 1 :\n" + secret1);
 log("Secret 2 :\n" + secret2);
 
-function ecdhAgreePrimeFuzzing(){
-	cryptopp.ecdh.prime.agree(rand(), {x: rand(), y: rand()}, 'secp256r1');
+if (useFuzzing){
+	function ecdhAgreePrimeFuzzing(){
+		cryptopp.ecdh.prime.agree(rand(), {x: rand(), y: rand()}, 'secp256r1');
+	}
+	try {
+		ecdhAgreePrimeFuzzing();
+		console.log('ECDH agreement method (with fuzzing) on prime curve didn\'t raise an exception');
+	} catch (e){}
 }
-try {
-	ecdhAgreePrimeFuzzing();
-	console.log('ECDH agreement method (with fuzzing) on prime curve didn\'t raise an exception');
-} catch (e){}
 
 //Testing ECDH on binary fields
 /*
